c6dddec5d4520fb4780099aff0f9ab5a08384012
tush-tr <tusharrajput604@gmail.com>
2023-03-29 03:38:36 +0530

    🎉 init(Next.js project): init

---
+++ src/Components/Header/index.js
@@ -0,0 +1,8 @@
+import styles from "./Header.module.css"
+export default function Header() {
+  return <header className={styles.header}>
+    <div className={styles.navBarLine}>
+        Hii
+    </div>
+  </header>;
+}
