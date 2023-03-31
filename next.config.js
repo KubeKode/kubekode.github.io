f67c2116e83a4cd43d97bd7fcd4497a46b4ad863
tush-tr <tusharrajput604@gmail.com>
2023-03-31 18:17:48 +0530

    ✨ feat(config): changed

---
+++ next.config.js
@@ -0,0 +1,29 @@
+// const repo = "kubekode.github.io";
+// const assetPrefix = `/${repo}/`;
+// const basePath = `/${repo}`;
+
+// module.exports = {
+//   assetPrefix: assetPrefix,
+//   basePath: basePath,
+// };
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
+
+// module.exports = nextConfig;
+// /**
+//  * @type {import('next').NextConfig}
+//  */
+// const nextConfig = {
+//   images: {
+//     loader: 'akamai',
+//     path: '',
+//   },
+//   assetPrefix: './',
+// };
+
+// export default nextConfig;
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,12 +1,15 @@
 import styles from "./Header.module.css";
 import Navbar from "./Navbar";
-import Logo from "../Logo";
+// import Logo from "../Logo";
+import Image from "next/image";
+
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.navBarLine}>
         <div>
-          <Logo />
+          {/* <Logo /> */}
+          <Image src="/logo.png" alt="KubeKode" width="130" height="100" priority="high" />
         </div>
         <Navbar />
       </div>
