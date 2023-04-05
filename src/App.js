dd665c44c6d0a56f5a00ae98c3b9f770018758f0
tush-tr <tusharrajput604@gmail.com>
2023-04-05 15:40:28 +0530

    react app init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Router></Router>
+    </div>
+  );
+}
+
+export default App;
