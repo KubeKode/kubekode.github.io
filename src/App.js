bea053d92b34dd9332c436082ad087160a264b92
tush-tr <tusharrajput604@gmail.com>
2023-04-09 15:44:30 +0530

    feat: Enhance Navbar
    Changed the pages array to an array of objects with two keys: label and link.
    Wrapped the navigation links inside the Link component from react-router-dom, using the link key from each object in the pages array as the 'to' prop of Link.
    Removed the MenuItem component from the Material-UI menu in favor of using a Link component inside a Button component.
    Changed the color of the AppBar background and added padding to it.
    Removed the borderBottom property from the AppBar.
    Removed the boxShadow property from the AppBar.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-
 import Home from "./pages/Home";
+import About from "./pages/About";
 import Header from "./components/Header";
+
+
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Header />
+    <Router>
+      <Header />
+      <div className="App">
         <Routes>
           <Route path="/" Component={Home} />
+          <Route path="/about" Component={About} />
         </Routes>
-      </Router>
-    </div>
+      </div>
+    </Router>
   );
 }
 
