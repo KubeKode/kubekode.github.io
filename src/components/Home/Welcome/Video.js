c2042bde14bc637ad70666fe2b5a13bbe5f6d12a
tush-tr <tusharrajput604@gmail.com>
2023-04-14 21:27:00 +0530

    ✨ feat(Home Page): Welcome

--- src/components/Home/Welcome/Video.js
+++ src/components/Home/Welcome/Video.js
@@ -1,22 +1,18 @@
 import ReactPlayer from "react-player";
-import PlayCircleFilledWhiteIcon from "@mui/icons-material/PlayCircleFilledWhite";
+import styles from "./welcome.module.css";
 const Video = () => {
   return (
-    <ReactPlayer
-      url="https://www.youtube.com/watch?v=p9hyNWXAC1c"
-      controls={true}
-      playIcon={PlayCircleFilledWhiteIcon}
-      style={{
-        boxShadow: "1px 1px 20px 6px rgb(0 0 0 / 30%)",
-      }}
-      config={
-          {
-              youtube:{
-                playerVars: { showinfo: 1 ,autoPlay: 1}
-              }
-          }
-      }
-    />
+    <div className={styles.player_wrapper}>
+      <ReactPlayer
+        url="https://www.youtube.com/watch?v=p9hyNWXAC1c"
+        // playing
+        width="100%"
+        height="100%"
+        style={{
+          boxShadow: "1px 1px 20px 6px rgb(0 0 0 / 30%)",
+        }}
+      />
+    </div>
   );
 };
 export default Video;
