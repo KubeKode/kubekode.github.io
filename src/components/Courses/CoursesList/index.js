043512e2abf6e264cea7c012e715b1fbda9b6b30
tush-tr <tusharrajput604@gmail.com>
2023-04-25 03:52:10 +0530

    api update

--- src/components/Courses/CoursesList/index.js
+++ src/components/Courses/CoursesList/index.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 
 const CardsList = () => {
   const [courses, setCourses] = useState([]);
-  const apiUrl = "https://script.googleusercontent.com/macros/echo?user_content_key=gYPjuEIsMInkIbuuL03uZfJkR32Nr_Qa7u0C2KbFfiE1Qa3vqlsA3-j-73e465jj65SnmjQqCx3Pxz1m7Mz-8VqHfVvj7tWim5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnLuUF7Z_-YWgZ9b5rXgJdXzfMrEGX0pdamPGVLCcN92de-W0SactNBfoEaVcVxDHGMyfzpMakdoMsZK7EmedLNfacBGtODc9ltz9Jw9Md8uu&lib=MXtcx8t5MfxcgoPV7nkaC8iD3pa3g3bgM"
+  const apiUrl = "https://script.googleusercontent.com/macros/echo?user_content_key=syVv518n5OwgpgcHSzl9_SEMg4Li2OYHfISsg_aQ65Fi-ttE1BkygCJAwMSNMpMpgfs3AhkmnDcwoUclVk3zyUwghW63uwWEm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnGs7kWtsxdIhbd-yUAbNSQfBreM10RyclkMmgwhBJZt9X32ioCho-rBl4JggHsl0JDakBIU3ZCfQSUy7J_qfNpLnMvPOaICEvQ&lib=MXtcx8t5MfxcgoPV7nkaC8iD3pa3g3bgM"
   const getData = async () => {
     const apiData = await fetch(apiUrl);
     const data = await apiData.json();
@@ -19,7 +19,7 @@ const CardsList = () => {
         {courses.map((card, index) => (
           <CourseCard
             title={card.title}
-            link={card.link}
+            link={card.course.link}
             image={card.image}
             content={card.content}
             index={index}
